2e51d6d93ece277b71b7c8f90f712e192fb46915
Pavlo Bratkevych <shel.concept@gmail.com>
2025-03-04 14:51:21 +0200

    add missing.

---
+++ UniverseGroupTestApp/Classes/TabBar/TabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  TabBarController.swift
+//  UniverseGroupTestApp
+//
+//  Created by Pavlo Bratkevych on 25.01.2025.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+  // MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupNavigationBarAppearance()
+  }
+  
+  // MARK: - Setup
+  private func setupNavigationBarAppearance() {
+    let appearance = UINavigationBarAppearance()
+    appearance.configureWithOpaqueBackground()
+    appearance.backgroundColor = AppColor.MainTabBar.navigationBarBG
+    appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+    
+    UINavigationBar.appearance().standardAppearance = appearance
+    UINavigationBar.appearance().scrollEdgeAppearance = appearance
+  }
+}
